0e858252be86526c586a0f18f3e01882749b9aad
sahuang <xiaohai.xu@zilliz.com>
2020-06-01 17:50:41 +0800

    tutorial
    Signed-off-by: sahuang <xiaohai.xu@zilliz.com>

--- core/src/index/thirdparty/faiss/tutorial/cpp/5-GPU.cpp
+++ core/src/index/thirdparty/faiss/tutorial/cpp/5-GPU.cpp
@@ -72,9 +72,9 @@ int main() {
     int number = 8;
 #endif
 
-    int d = 512;                            // dimension
-    int nq = 10;                        // nb of queries
-    int nprobe = 1;
+    int d = 128;                            // dimension
+    int nq = 10000;                        // nb of queries
+    int nprobe = 20;
     float *xq = new float[d * nq];
     for(int i = 0; i < nq; i++) {
         for(int j = 0; j < d; j++) {
@@ -85,49 +85,44 @@ int main() {
 
     faiss::gpu::StandardGpuResources res;
 
-    int k = 8;
+    int k = 4;
     std::shared_ptr<faiss::Index> gpu_index_ivf_ptr;
 
-    const char* index_description = "IVF16384,SQ8";
+    const char* index_description = "IVF4096,Flat";
 //     const char* index_description = "IVF3276,SQ8";
 
     faiss::Index *cpu_index = nullptr;
     faiss::IndexIVF* cpu_ivf_index = nullptr;
-    if((access(filename,F_OK))==-1) {
-        // create database
-        long nb = 500000;                       // database size
+
+    long nb = 5000000;                       // database size
 //        printf("-----------------------\n");
-        long size = d * nb;
-        float *xb = new float[size];
-        memset(xb, 0, size * sizeof(float));
-        printf("size: %ld\n", (size * sizeof(float)) );
-        for(long i = 0; i < nb; i++) {
-            for(long j = 0; j < d; j++) {
-                float rand = drand48();
-                xb[d * i + j] = rand;
-            }
+    long size = d * nb;
+    float *xb = new float[size];
+    memset(xb, 0, size * sizeof(float));
+    printf("size: %ld\n", (size * sizeof(float)) );
+    for(long i = 0; i < nb; i++) {
+        for(long j = 0; j < d; j++) {
+            float rand = drand48();
+            xb[d * i + j] = rand;
         }
-
-        faiss::Index *ori_index = faiss::index_factory(d, index_description, faiss::METRIC_L2);
-        auto device_index = faiss::gpu::index_cpu_to_gpu(&res, 0, ori_index);
-
-        gpu_index_ivf_ptr = std::shared_ptr<faiss::Index>(device_index);
-
-        assert(!device_index->is_trained);
-        device_index->train(nb, xb);
-        assert(device_index->is_trained);
-        device_index->add(nb, xb);  // add vectors to the index
-
-        printf("is_trained = %s\n", device_index->is_trained ? "true" : "false");
-        printf("ntotal = %ld\n", device_index->ntotal);
-
-        cpu_index = faiss::gpu::index_gpu_to_cpu ((device_index));
-        faiss::write_index(cpu_index, filename);
-        printf("index.index is stored successfully.\n");
-        delete [] xb;
-    } else {
-        cpu_index = faiss::read_index(filename);
     }
+
+    faiss::Index *ori_index = faiss::index_factory(d, index_description, faiss::METRIC_L2);
+    auto device_index = faiss::gpu::index_cpu_to_gpu(&res, 0, ori_index);
+
+    gpu_index_ivf_ptr = std::shared_ptr<faiss::Index>(device_index);
+
+    assert(!device_index->is_trained);
+    device_index->train(nb, xb);
+    assert(device_index->is_trained);
+    device_index->add(nb, xb);  // add vectors to the index
+
+    printf("is_trained = %s\n", device_index->is_trained ? "true" : "false");
+    printf("ntotal = %ld\n", device_index->ntotal);
+
+    cpu_index = faiss::gpu::index_gpu_to_cpu ((device_index));
+    faiss::write_index(cpu_index, filename);
+    printf("index.index is stored successfully.\n");
 
     cpu_ivf_index = dynamic_cast<faiss::IndexIVF*>(cpu_index);
     if(cpu_ivf_index != nullptr) {
@@ -140,55 +135,97 @@ int main() {
         delete tmp_index;
     };
 
-    auto gpu_executor = [&](int device_id, faiss::gpu::GpuClonerOptions* option) {
-    auto tmp_index = faiss::gpu::index_cpu_to_gpu(&res, device_id, cpu_index, option);
-    delete tmp_index;
-    double t0 = getmillisecs ();
-    {
-        // cpu to gpu
+    auto gpu_executor = [&](int device_id, float *xb, faiss::gpu::GpuClonerOptions* option) {
+        auto tmp_index = faiss::gpu::index_cpu_to_gpu(&res, device_id, cpu_index, option);
+        delete tmp_index;
+        double t0 = getmillisecs ();
         option->allInGpu = true;
+        
+        {
+            // cpu to gpu
+            option->allInGpu = true;
 
-        tmp_index = faiss::gpu::index_cpu_to_gpu(&res, device_id, cpu_index, option);
-        gpu_index_ivf_ptr = std::shared_ptr<faiss::Index>(tmp_index);
-    }
-    double t1 = getmillisecs ();
-    printf("CPU to GPU loading time: %0.2f\n", t1 - t0);
-
-    {
-        long *I = new long[k * nq];
-        float *D = new float[k * nq];
-	if(option->allInGpu) {
-	    faiss::gpu::GpuIndexIVF* gpu_index_ivf =
-		dynamic_cast<faiss::gpu::GpuIndexIVF*>(gpu_index_ivf_ptr.get());
-	    gpu_index_ivf->setNumProbes(nprobe);
-        for(long i = 0; i < 1; ++ i) {
-            double t2 = getmillisecs();
-            gpu_index_ivf_ptr->search(nq, xq, k, D, I);
-            double t3 = getmillisecs();
-            printf("* GPU: %d, execution time: %0.2f\n", device_id, t3 - t2);
+            tmp_index = faiss::gpu::index_cpu_to_gpu(&res, device_id, cpu_index, option);
+            gpu_index_ivf_ptr = std::shared_ptr<faiss::Index>(tmp_index);
         }
-	} else {
-	    faiss::IndexIVFScalarQuantizer* index_ivf =
-		dynamic_cast<faiss::IndexIVFScalarQuantizer*>(gpu_index_ivf_ptr.get());
-	    index_ivf->nprobe = nprobe;
-        for(long i = 0; i < 1; ++ i) {
-            double t2 = getmillisecs();
-	        index_ivf->search(nq, xq, k, D, I);
-            double t3 = getmillisecs();
-            printf("- GPU: %d, execution time: %0.2f\n", device_id, t3 - t2);
-        }
-	}
+        
+        double t1 = getmillisecs ();
+        printf("CPU to GPU loading time: %0.2f\n", t1 - t0);
 
-        // print results
+        {
+            long *I = new long[k * nq];
+            float *D = new float[k * nq];
+        if(option->allInGpu) {
+            faiss::gpu::GpuIndexIVF* gpu_index_ivf =
+            dynamic_cast<faiss::gpu::GpuIndexIVF*>(gpu_index_ivf_ptr.get());
+            gpu_index_ivf->setNumProbes(nprobe);
+            for(long i = 0; i < 1; ++ i) {
+                double t2 = getmillisecs();
+
+                auto xxx = faiss::gpu::index_cpu_to_gpu(&res, device_id, cpu_index, option);
+                t2 = getmillisecs();
+                auto cpuindex = faiss::gpu::index_gpu_to_cpu_without_codes (xxx);
+
+                printf("gpu to cpu without codes time: %0.2f\n", getmillisecs() - t2);
+                t2 = getmillisecs();
+
+                auto cpuindex2 = faiss::gpu::index_gpu_to_cpu (xxx);
+
+                printf("gpu to cpu time: %0.2f\n", getmillisecs() - t2);
+                t2 = getmillisecs();
+
+                faiss::IndexIVF* ivf_index = dynamic_cast<faiss::IndexIVF*>(cpuindex);
+                ivf_index->nprobe = nprobe;
+                ivf_index->search_without_codes(nq, xq, xb, k, D, I);
+                printf("cpu without codes execution time: %0.2f\n", getmillisecs() - t2);
+                t2 = getmillisecs();
+
+                // cpu to gpu
+                auto cpu_to_gpu = faiss::gpu::index_cpu_to_gpu_without_codes(&res, device_id, cpu_index, xb, option);
+                printf("cpu to gpu without codes time: %0.2f\n", getmillisecs() - t2);
+                t2 = getmillisecs();
+                auto cpu_to_gpu_ivf_ptr = std::shared_ptr<faiss::Index>(cpu_to_gpu);
+                faiss::gpu::GpuIndexIVF* gpu_index_ivf = dynamic_cast<faiss::gpu::GpuIndexIVF*>(cpu_to_gpu_ivf_ptr.get());
+                gpu_index_ivf->setNumProbes(nprobe);
+                gpu_index_ivf->search_without_codes(nq, xq, xb, k, D, I);
+                printf("gpu without codes execution time: %0.2f\n", getmillisecs() - t2);
+                printf("I=\n");
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < k; j++)
+                printf("%5ld ", I[i * k + j]);
+            printf("\n");
+        }
+
+        printf("D=\n");
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < k; j++)
+                printf("%.5f ", D[i * k + j]);
+            printf("\n");
+        }
+                t2 = getmillisecs();
+            }
+        } else {
+            faiss::IndexIVFScalarQuantizer* index_ivf =
+            dynamic_cast<faiss::IndexIVFScalarQuantizer*>(gpu_index_ivf_ptr.get());
+            index_ivf->nprobe = nprobe;
+            for(long i = 0; i < 1; ++ i) {
+                double t2 = getmillisecs();
+                index_ivf->search(nq, xq, k, D, I);
+                double t3 = getmillisecs();
+                printf("- GPU: %d, execution time: %0.2f\n", device_id, t3 - t2);
+            }
+        }
+
+            // print results
 #if PRINT_RESULT
-        print_result("GPU", number, k, nq, I);
+            print_result("GPU", number, k, nq, I);
 #endif
-        delete [] I;
-        delete [] D;
-    }
-    double t4 = getmillisecs();
+            delete [] I;
+            delete [] D;
+        }
+        double t4 = getmillisecs();
 
-    printf("GPU:%d total time: %0.2f\n", device_id, t4 - t0);
+        printf("GPU:%d total time: %0.2f\n", device_id, t4 - t0);
 
     };
     printf("----------------------------------\n");
@@ -204,6 +241,19 @@ int main() {
         cpu_index->search(nq, xq, k, D, I);
         double t5 = getmillisecs();
         printf("CPU execution time: %0.2f\n", t5 - t4);
+        printf("I=\n");
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < k; j++)
+                printf("%5ld ", I[i * k + j]);
+            printf("\n");
+        }
+
+        printf("D=\n");
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < k; j++)
+                printf("%.5f ", D[i * k + j]);
+            printf("\n");
+        }
 #if PRINT_RESULT
         print_result("CPU", number, k, nq, I);
 #endif
@@ -216,16 +266,16 @@ int main() {
     }
 
     faiss::gpu::GpuClonerOptions option0;
-    faiss::gpu::GpuClonerOptions option1;
+    //faiss::gpu::GpuClonerOptions option1;
 
 //    init_gpu(0, &option0);
 //    init_gpu(1, &option1);
 
 //    double tx = getmillisecs();
-    std::thread t1(gpu_executor, 0, &option0);
-    std::thread t2(gpu_executor, 1, &option1);
+    std::thread t1(gpu_executor, 0, xb, &option0);
+    //std::thread t2(gpu_executor, 1, &option1);
     t1.join();
-    t2.join();
+    //t2.join();
 //    double ty = getmillisecs();
 //    printf("Total GPU execution time: %0.2f\n", ty - tx);
 
